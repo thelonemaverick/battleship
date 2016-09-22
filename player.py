63a316efb3bc13aeb2c06ef3abcdb8e0331815ed
abetker <adam.betker@gmail.com>
2016-09-22 15:35:08 -0500

    Initial commit

---
+++ player.py
@@ -0,0 +1,68 @@
+tile = """
+         ==== ==== ==== ==== ====
+         |{}| |{}| |{}| |{}| |{}|
+         ==== ==== ==== ==== ====
+         |{}| |{}| |{}| |{}| |{}|
+         ==== ==== ==== ==== ====
+         |{}| |{}| |{}| |{}| |{}|
+         ==== ==== ==== ==== ====
+         |{}| |{}| |{}| |{}| |{}|
+         ==== ==== ==== ==== ====
+         |{}| |{}| |{}| |{}| |{}|
+         ==== ==== ==== ==== ====
+
+         """
+
+class Player:
+
+    def __init__(self):
+        # Ask player their name
+
+        name = self.ask_name()
+        print("My name is" + name)
+        # Draw empty board
+        # Ask to add Ships
+        # five ships place each one and redraw
+
+        # create subclass for ships
+        # Owner
+        # Position
+        # Horizontal or Vertical
+        # function for list of coordinates
+
+    def ask_name(self):
+
+        return input("What is your player name?").title()
+
+    def draw_empty_board(self):
+        pass
+
+
+# = Player()
+
+#player2 = Player()
+
+full_board = [(x,y) for y in range(1,10) for x in 'ABCDEFGHIJ']
+
+print(full_board)
+
+hits = [('A', 1), ('B', 3), ('C', 4), ('D', 1), ('E', 6), ('F', 9)]
+
+count = 1
+row_count = 1
+board = """1 """
+for space in full_board:
+
+    if space in hits:
+        board += "X"
+    else:
+        board += "O"
+    count += 1
+    if count == 10:
+        row_count += 1
+        board += """
+""" + str(row_count)+""" """
+        count = 1
+
+print(board)
+
