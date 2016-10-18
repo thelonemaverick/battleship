b51ff376163513965672fb0e6e7a570f6d7542c6
abetker <adam.betker@gmail.com>
2016-10-18 16:46:41 -0500

    Drew a sample game board.  1st pass.

--- battleship.py
+++ battleship.py
@@ -16,21 +16,62 @@ HIT = '*'
 SUNK = '#'
 
 
-def clear_screen():
-    print("\033c", end="")
+
+class Board:
+    def clear_screen():
+        print("\033c", end="")
+
+    def print_board_heading():
+        print("   " + " ".join([chr(c) for c in range(ord('A'), ord('A') + BOARD_SIZE)]))
+
+    def print_board(board):
+        print_board_heading()
+
+        row_num = 1
+        for row in board:
+            print(str(row_num).rjust(2) + " " + (" ".join(row)))
+            row_num += 1
+
+    def game_play(self):
+        while True:
+            Board.clear_screen()
+            Player1.turn()
+            Board.clear_screen()
+            Player2.turn()
 
 
-def print_board_heading():
-    print("   " + " ".join([chr(c) for c in range(ord('A'), ord('A') + BOARD_SIZE)]))
+class Player:
+    def __init__(self):
+        # Ask player their name
+        # Ask to add Ships
+        self.name = input("What is your player name?").title()
+        Ship.build_ships(self, SHIP_INFO)
+
+    def turn(self):
+        input("{}, Select a coordinate to hit (A,1)".format(self.name))
 
 
-def print_board(board):
-    print_board_heading()
+class Ship(Player):
 
-    row_num = 1
-    for row in board:
-        print(str(row_num).rjust(2) + " " + (" ".join(row)))
-        row_num += 1
+    def __init__(self):
 
+        # self.ship_name = ship_name
+        # self.coordinate = coordinate
+        # self.orientation = orientation
+        # self.ship_length = ship_length
+        print("I made a ship{}".format(self.name))
 
-print_board_heading()
+    def build_ships(self, SHIP_INFO):
+        for ship in SHIP_INFO:
+            self.ship_name = ship[0]
+            self.ship_length = ship[1]
+            coordinate = input("{}, where would you like to place your {} ({})? (A,1)".format(self.name, self.ship_name, self.ship_length))
+            orientation = input("Place the {} vertically or horizontally (v/h)".format(self.ship_name))
+            print("You just placed a {}".format(self.ship_name + coordinate + orientation))
+
+Board_Play = Board()
+Player1 = Player()
+Board.clear_screen()
+Player2 = Player()
+Board.clear_screen()
+Board_Play.game_play()
--- player.py
+++ player.py
@@ -1,63 +1,109 @@
-class Player:
-    player_name = input("What is your player name?").title()
-    full_board = [(x, y) for y in range(1, 11) for x in 'ABCDEFGHIJ']
-    hits = [('I', 1), ('J', 1), ('A', 2), ('B', 2), ('C', 2), ('D', 2), ('E', 2)]
-    ship_info = [
-        ("Aircraft Carrier", 5),
-        ("Battleship", 4),
-        ("Submarine", 3),
-        ("Cruiser", 3),
-        ("Patrol Boat", 2)
-    ]
-    def __init__(self):
-        # Ask player their name
-        # Ask to add Ships
+if __name__ == '__main__':
 
-        input("What is your player name?")
-        input("Where would you like to place your ship? (x,x)")
-        input("Place it vertically or horizontally (v/h)")
+    BOARD_WIDTH = "ABCDEFGHIJ"
 
+    class Ship:
 
-        # five ships place each one and redraw
-        self.draw_board()
-        # create subclass for ships
-        # Owner
-        # Position
-        # Horizontal or Vertical
-        # function for list of coordinates
+        def __init__(self, coordinate, orientation, length):
+            self.coordinate = coordinate
+            self.orientation = orientation
+            self.length = length
 
-    def ask_name(self):
+        def set_ship(self, orientation, coordinate, ship_length):
+            # h/v plus length, compensate for edges, return coordinates [('A', 2), ('B', 2), ('C', 2), ('D', 2), ('E', 2)]
 
-        return input("What is your player name?").title()
+            if orientation == "h":
+                strposition = BOARD_WIDTH.index(coordinate[0])
+                if (strposition + ship_length) > 1:
+                    row_position = len(BOARD_WIDTH) - ship_length
+                    row_coordinates = [(coordinate)]
+                    count = 0
+                    while count < ship_length:
+                        row_coordinates = (BOARD_WIDTH[strposition + count], coordinate[1])
+                        count = count + 1
+                    print(row_coordinates)
 
-    print(full_board)
+                else:
+                    row_position = strposition
+                    row_coordinates = []
 
-    def draw_board(self):
-        print("Player -"+self.player_name)
-        print("  " + "  ".join([chr(c) for c in range(ord('A'), ord('A') + 10)]))
-        count = 0
-        row_count = 1
-        board = """1"""
-        for space in self.full_board:
-
-            if space in self.hits:
-                board += " X "
+            elif orientation == "v":
+                # count out on a column
+                pass
             else:
-                board += " O "
-            count += 1
-            if count == 10:
-                row_count += 1
+                print("Your orientation was invalid")
 
-                if row_count < 11:
-                    board += """
-""" + str(row_count)
 
-                count = 0
-        print(board)
 
-Player()
+        def check_other_ships(self, placed_coordinate):
+            # checking for collision with other ships
+            # returns a string "ship placed successfully" or "Another ship is there, try again"
+            pass
 
-# player2 = Player()
+    class Player:
+        player_name = input("What is your player name?").title()
+        full_board = [(x, y) for y in range(1, 11) for x in BOARD_WIDTH]
+        hits = [('I', 1), ('J', 1), ('A', 2), ('B', 2), ('C', 2), ('D', 2), ('E', 2)]
 
+        ship_info = [
+            ("Aircraft Carrier", 5),
+            ("Battleship", 4),
+            ("Submarine", 3),
+            ("Cruiser", 3),
+            ("Patrol Boat", 2)
+        ]
+        def __init__(self):
+
+            # Ask player their name
+            # Ask to add Ships
+
+            input("What is your player name?")
+            c = input("Where would you like to place your ship? (A,1)")
+            o = input("Place it vertically or horizontally (v/h)")
+
+            Ship(c, o, 4)
+
+
+            # five ships place each one and redraw
+
+            self.draw_board()
+            # create subclass for ships
+            # Owner
+            # Position
+            # Horizontal or Vertical
+            # function for list of coordinates
+
+        def ask_name(self):
+
+            return input("What is your player name?").title()
+
+        print(full_board)
+
+        def draw_board(self):
+            print("Player -"+self.player_name)
+            print("  " + "  ".join([chr(c) for c in range(ord('A'), ord('A') + 10)]))
+            count = 0
+            row_count = 1
+            board = """1"""
+            for space in self.full_board:
+
+                if space in self.hits:
+                    board += " X "
+                else:
+                    board += " O "
+                count += 1
+                if count == 10:
+                    row_count += 1
+
+                    if row_count < 11:
+                        board += """
+    """ + str(row_count)
+
+                    count = 0
+            print(board)
+
+    Player()
+
+    # player2 = Player()
 
 
